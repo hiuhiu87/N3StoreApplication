769b7a94e3d565a03c988831a1dfe660c610fa4a
Nguyễn Minh Hiếu <minhhieu8723@gmail.com>
2023-11-03 23:07:25 +0700

    upload base package

---
+++ src/app/service/Test.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app.service;
+
+import app.model.*;
+
+/**
+ *
+ * @author Admin
+ */
+public class Test {
+    
+}
